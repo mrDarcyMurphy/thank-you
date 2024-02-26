50f5fd2a382078a3d7f3a9baf7db64907b10bd13
Mr. Darcy Murphy <darcy.murphy@loudbit.com>
2024-02-25 20:27:45 -0600

    use LeagueScript; centered

--- src/App.tsx
+++ src/App.tsx
@@ -9,13 +9,10 @@ const magenta = "236,0,140"
 const yellow = "255,242,140"
 const white = "255,255,255"
 const blendingValue = "screen" // overlay | screen | hard-light
-
-const message = "Thank you!"
-
 const colors = [`rgb(${cyan})`, `rgb(${magenta})`, `rgb(${yellow})`]
+const message = <>Thank<br />you!</>
 
 function App() {
-
   return (
     <>
       <BackgroundGradientAnimation
@@ -31,24 +28,21 @@ function App() {
         className="absolute"
       />
       <div className="absolute top-0">
-        <WavyBackground waveWidth={5} colors={colors} className="max-w-4xl mx-auto pb-40 absolute top-0" />
+        <WavyBackground waveWidth={3} colors={colors} className="max-w-4xl mx-auto pb-40 absolute top-0" />
       </div>
       <div className="absolute top-0 w-full h-full z-20">
-        <CardContainer className="inter-var w-full h-full">
-          <CardBody className="relative group/card w-full h-full font-knewave pointer-events-none thank-you">
-            <CardItem translateZ="50" className="absolute text-shadow">{message}</CardItem>
-            <CardItem translateZ="55" className="absolute text-shadow">{message}</CardItem>
-            <CardItem translateZ="60" className="absolute text-shadow">{message}</CardItem>
-            <CardItem translateZ="65" className="absolute text-shadow">{message}</CardItem>
-            <CardItem translateZ="70" className="absolute text-shadow">{message}</CardItem>
-            <CardItem translateZ="75" className="absolute text-shadow">{message}</CardItem>
-            <CardItem translateZ="80" className="absolute text-shadow">{message}</CardItem>
-            <CardItem translateZ="85" className="absolute text-shadow">{message}</CardItem>
-            <CardItem translateZ="90" className="absolute text-shadow">{message}</CardItem>
-            <CardItem translateZ="95" className="absolute text-shadow">{message}</CardItem>
-            <CardItem as={"h1"} translateZ="100" className="absolute text-shadow text-white">
-              {message}
-            </CardItem>
+        <CardContainer className="w-full">
+          <CardBody className="group/card pointer-events-none">
+            <div className="h-full flex items-center justify-center text-center thank-you font-league-script">
+              <CardItem translateZ="65" className="absolute p-4 opacity-95">{message}</CardItem>
+              <CardItem translateZ="70" className="absolute p-4 opacity-95">{message}</CardItem>
+              <CardItem translateZ="75" className="absolute p-4 opacity-95">{message}</CardItem>
+              <CardItem translateZ="80" className="absolute p-4 opacity-95">{message}</CardItem>
+              <CardItem translateZ="85" className="absolute p-4 opacity-95">{message}</CardItem>
+              <CardItem translateZ="90" className="absolute p-4 opacity-95">{message}</CardItem>
+              <CardItem translateZ="95" className="absolute p-4 opacity-95">{message}</CardItem>
+              <CardItem as={"h1"} translateZ="100" className="absolute p-4 text-glow">{message}</CardItem>
+            </div>
           </CardBody>
         </CardContainer>
       </div>
